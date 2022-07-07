fbd3f8edc4e30153e39941e114e15ac297f2f6a2
Luke Spragg <luke@spragg.me>
2022-07-07 15:41:57 -0400

    Revert "Update SQLite library"
    This reverts commit e75147242c6421fb7d443a0897f001b99fdc73df.

--- src/SQLiteExtension.cs
+++ src/SQLiteExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Oxide.Core.Extensions;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace Oxide.Core.SQLite
@@ -44,25 +45,20 @@ namespace Oxide.Core.SQLite
             {
                 string extDir = Interface.Oxide.ExtensionDirectory;
                 string configPath = Path.Combine(extDir, "System.Data.SQLite.dll.config");
-                File.WriteAllText(configPath, $"<configuration>\n<dllmap dll=\"SQLite.Interop\" target=\"{extDir}/x86/libSQLite.Interop.so\" os=\"!windows,osx\" cpu=\"x86\" />\n" +
-                    $"<dllmap dll=\"SQLite.Interop\" target=\"{extDir}/x64/libSQLite.Interop.so\" os=\"!windows,osx\" cpu=\"x86-64\" />\n</configuration>");
+                if (File.Exists(configPath) && !new[] { "target=\"x64", "target=\"./x64" }.Any(File.ReadAllText(configPath).Contains))
+                {
+                    return;
+                }
+
+                File.WriteAllText(configPath, $"<configuration>\n<dllmap dll=\"sqlite3\" target=\"{extDir}/x86/libsqlite3.so\" os=\"!windows,osx\" cpu=\"x86\" />\n" +
+                    $"<dllmap dll=\"sqlite3\" target=\"{extDir}/x64/libsqlite3.so\" os=\"!windows,osx\" cpu=\"x86-64\" />\n</configuration>");
             }
         }
 
         /// <summary>
         /// Loads this extension
         /// </summary>
-        public override void Load()
-        {
-            Manager.RegisterLibrary("SQLite", new Libraries.SQLite());
-
-            Cleanup.Add("RustDedicated_Data\\Managed\\x64\\libsqlite3");
-            Cleanup.Add("RustDedicated_Data\\Managed\\x64\\libsqlite3.so");
-            Cleanup.Add("RustDedicated_Data\\Managed\\x64\\sqlite3.dll");
-            Cleanup.Add("RustDedicated_Data\\Managed\\x86\\libsqlite3");
-            Cleanup.Add("RustDedicated_Data\\Managed\\x86\\libsqlite3.so");
-            Cleanup.Add("RustDedicated_Data\\Managed\\x86\\sqlite3.dll");
-        }
+        public override void Load() => Manager.RegisterLibrary("SQLite", new Libraries.SQLite());
 
         /// <summary>
         /// Loads plugin watchers used by this extension
@@ -77,11 +73,6 @@ namespace Oxide.Core.SQLite
         /// </summary>
         public override void OnModLoad()
         {
-            AssemblyName assemblyName = AssemblyName.GetAssemblyName(Path.Combine(Interface.Oxide.ExtensionDirectory, "System.Data.SQLite.dll"));
-            if (assemblyName != null)
-            {
-                Interface.Oxide.LogInfo($"SQLite connector version: {assemblyName.Version}");
-            }
         }
     }
 }
